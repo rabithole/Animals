93db911169e47e31e4bc063776e302d87a4e6c6c
Robert Kulp <pedalinon@gmail.com>
2019-05-20 10:16:30 -0700

    Initial commit for react tooling practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Animals from './components/animalsData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      animals: animals
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h1>Welcome to the zoo, we have Animals here!</h1>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+          </p>   
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/animalsData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Animals extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			nothing: []
+		};
+	}
+
+	render() {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
+
+export default Animals;
